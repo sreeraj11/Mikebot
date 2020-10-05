dafc10a152d0bae39843ceb689d7abd7931f9bc4
sreeraj11 <51787957+sreeraj11@users.noreply.github.com>
2020-10-05 18:27:24 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -4,8 +4,8 @@ from userbot.utils import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = ""
-pm_caption = "`Yo Bitch I am Alive🖕🖕 `\n\n"
+
+pm_caption = "`Yo Bitch I am Alive🖕 `\n\n"
 
 
 
@@ -15,5 +15,5 @@ async def amireallyalive(alive):
     chat = await alive.get_chat()
     await alive.delete()
     """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await borg.send_message(alive.chat_id, pm_caption)
     await alive.delete() 
